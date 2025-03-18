965611da81dadffdec3ef3b8b063ec552a771745
ＪＯＡＯ ＰＥＤＲＯ <reisriosjoaopedro@gmail.com>
2025-03-18 20:02:44 -0300

    Update listasordenadas.cs

--- listasordenadas/listasordenadas.cs
+++ listasordenadas/listasordenadas.cs
@@ -29,3 +29,42 @@ Fonte: Adaptado de Próprio Autor
 
 A utilização dessa estrutura de dados no desenvolvimento de jogos é importante para situações nas quais a ordem dos elementos é importante para a lógica e para
 as regras de um jogo.
+Listas ordenadas !!!
+
+
+// Figura 11 - Método de inserção em uma lista ordenada encadeada
+
+
+public void insereNoOrdenado(Node novo)
+{
+    Node aux;
+
+    // Verificando se a lista está vazia
+    if (primeiro == null || contaNos() == 0)
+    {
+        novo.Prox = primeiro;
+        primeiro = novo;
+    }
+    else
+    {
+        aux = primeiro;
+        int valorNovo = Convert.ToInt32(novo.Item); // Usar Item e converter corretamente
+        int valorAux = Convert.ToInt32(aux.Item);   // Usar Item e converter corretamente
+
+        // Lógica para encontrar o ponto de inserção
+        if (valorNovo < valorAux)
+        {
+            novo.Prox = primeiro;
+            primeiro = novo;
+        }
+        else
+        {
+            while (aux.Prox != null && Convert.ToInt32(aux.Prox.Item) < valorNovo)
+            {
+                aux = aux.Prox;
+            }
+            novo.Prox = aux.Prox;
+            aux.Prox = novo;
+        }
+    }
+}
